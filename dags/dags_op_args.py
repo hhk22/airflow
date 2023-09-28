1849cfa848a4e8e64b4ecf8c405d8fe103be025b
Hyeonghwan <khhh9401@gmail.com>
2023-09-28 20:12:12 +0900

    op_args for test upload

---
+++ dags/dags_op_args.py
@@ -0,0 +1,23 @@
+import datetime
+import random
+import pendulum
+
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.python import PythonOperator
+from common.common_func import regist
+
+with DAG(
+    dag_id="dags_op_args",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 9, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    regist_t1 = PythonOperator(
+        task_id="regist_t1",
+        python_callable=regist,
+        op_args=["hjkim", "man", "kr", "seoul"]
+    )
+
+    regist_t1
--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,3 +1,6 @@
 
 def get_sftp():
     print('start sftp job')
+
+def regist(name, sex, *args):
+    print(f"name:{name}, sex:{sex}, options: {args}")
