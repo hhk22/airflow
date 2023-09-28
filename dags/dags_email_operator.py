e89ed61b3e1789d9804f369610e37a4f62ae47fe
Hyeonghwan <khhh9401@gmail.com>
2023-09-28 17:47:11 +0900

    bug fix

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -6,7 +6,7 @@ from airflow import DAG
 from airflow.operators.email import EmailOperator
 
 with DAG(
-    dags_id="dags_email_operator",
+    dag_id="dags_email_operator",
     schedule="0 8 1 * *",
     start_date=pendulum.datetime(2023, 9, 1, tz="Asia/Seoul"),
     catchup=False
