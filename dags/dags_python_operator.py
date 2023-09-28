fe4628176a128dff84466a2312a2d2b72ff82f68
Hyeonghwan <khhh9401@gmail.com>
2023-09-28 17:59:19 +0900

    add python basic op

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -6,7 +6,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 with DAG(
-    dag_id="dags_bash_operator",
+    dag_id="dags_python_operator",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 9, 1, tz="Asia/Seoul"),
     catchup=False,
