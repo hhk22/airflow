6b03ccb07e2db3319a3c8aeafa8f00c941f109cc
Hyeonghwan <khhh9401@gmail.com>
2023-09-30 09:56:40 +0900

    python_bash_with_xcom

---
+++ dags/dags_python_bash_with_xcom.py
@@ -0,0 +1,32 @@
+import datetime
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_python_bash_with_xcom",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 9, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    @task(task_id="python_bash")
+    def python_push_xcom():
+        return {
+            "status": "Good",
+            "data": [1, 2, 3],
+            "options_cnt": 100
+        }
+
+    bash_pull = BashOperator(
+        task_id="bash_pull",
+        env={
+            "STATUS": "{{ti.xcom_pull(task_id='python_bash')['status']}}"
+            "DATA": "{{ti.xcom_pull(task_id='python_bash')['data']}}"
+            "OPTIONS_CNT": "{{ti.xcom_pull(task_id='python_bash')['options_cnt']}}"
+        },
+        bash_command="echo $STATUS && echo $DATA && echo $OPTIONS_CNT"
+    )
+
+    python_push_xcom() >> bash_pull
