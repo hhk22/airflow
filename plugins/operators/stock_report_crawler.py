c1c8d53bf890477558cc25a1eed1aeee0fec549a
Hyeonghwan <khhh9401@gmail.com>
2024-01-10 23:34:44 +0900

    test

--- plugins/operators/stock_report_crawler.py
+++ plugins/operators/stock_report_crawler.py
@@ -14,16 +14,15 @@ class FinanceReportOperator(BaseOperator):
     
     def execute(self, context):
         date: datetime = context['data_interval_start']
-        print("now" + date.strftime("%Y.%m.%d"))
-        # self._get_report_list()
+        self._get_report_list(date=date.strftime("%Y.%m.%d")[2:])
 
-    def _get_report_list(self, date = ""):
+    def _get_report_list(self, date):
         response: Response = requests.get(self.urlparse.geturl())
         bs = BeautifulSoup(response.text, "lxml")
         table_tag: Tag = bs.find("table")
         for row in table_tag.find_all("tr"):
             date_tag = row.find(attrs={"class": "date"})
-            if not date_tag:
+            if not date_tag or date_tag.text != date:
                 continue
 
             tags: list[Tag] = row.find_all("a")
